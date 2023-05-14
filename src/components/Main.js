df1694825ebc723290cf84b7a5e4a5af29af16af
hachicombu <frefg25+git@gmail.com>
2023-05-14 22:39:59 +0900

    mainコンポーネント実装

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,4 +1,22 @@
 import "./Main.css";
-export const Main = () => {
-  return <div>Main</div>;
+export const Main = ({ activeNote }) => {
+  // ノートがない・初期表示
+  if (!activeNote) {
+    return <div className="no-active-note">ノートが選択されていません</div>;
+  }
+  return (
+    <div className="app-main">
+      <div className="app-main-note-edit">
+        <input type="text" />
+        <textarea placeholder="ノート内容を記入"></textarea>
+      </div>
+      <div className="app-main-note-preview">
+        <div className="preview-title">{activeNote.title}</div>
+        <div className="markdown-preview">
+          {activeNote.content}
+          {activeNote.id}
+        </div>
+      </div>
+    </div>
+  );
 };
