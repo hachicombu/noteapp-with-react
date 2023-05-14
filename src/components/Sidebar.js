c132e366c53491f8292a7e4fba33b18a9ac85ee6
hachicombu <frefg25+git@gmail.com>
2023-05-14 19:14:00 +0900

    sidebarスタイル追加

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,4 +1,21 @@
 import "./Sidebar.css";
 export const Sidebar = () => {
-  return <div>Sidebar</div>;
+  return (
+    <div className="app-sidebar">
+      <div className="app-sidebar-header">
+        <h1>ノート</h1>
+        <button>追加</button>
+      </div>
+      <div className="app-sidebar-notes">
+        <div className="app-sidebar-note">
+          <div className="sidebar-note-title">
+            <strong>タイトル</strong>
+            <button>削除</button>
+          </div>
+          <p>ノートの内容です</p>
+          <small>最後の修正日xx/xx</small>
+        </div>
+      </div>
+    </div>
+  );
 };
